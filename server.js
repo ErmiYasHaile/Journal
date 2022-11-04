a09bbe7ffd7ef7bca5c9c07123c9feeca9a69752
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-11-03 23:21:24 -0700

    mongoose connected

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 require('dotenv').config()
 
 const PORT = process.env.PORT
+
+
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+// DATABASE CONNECTION
+const db = mongoose.connection
+db.on("error", (err) => console.log(err.message + " is mongo not running?"))
+db.on("connected", () => console.log("mongo connected"))
+db.on("disconnected", () => console.log("mongo disconnected"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Listener
 app.listen(PORT,()=>console.log(`Live on port ${PORT} and still working`))
